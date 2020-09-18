24039328e90eddda1734eb99d2af397290143800
EDEN <EDBEK@YAHOO.COM>
2020-09-18 18:17:51 -0500

    Added a login button to authenticate user with Spotify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import hash from "./hash";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const clientId = "a7cea9e91767484796676a5037d4375c";
+const redirectUri = "http://localhost:3000";
+const scopes = ["user-read-currently-playing", "user-read-playback-state"];
+
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+  class App extends Component {
+    componentDidMount() {
+      // Set token
+      let _token = hash.access_token;
+      if (_token) {
+        // Set token
+        this.setState({
+          token: _token
+        });
+      }
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>
+          )}
+          {this.state.token && (
+            // Spotify Player Will Go Here In the Next Step
+          )}
+          </header>
+        </div>
+      );
+      }
+    }
+    
 
 export default App;
