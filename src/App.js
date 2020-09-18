fdf41ee546e88668f1dee1284db6b54424bcb6d2
EDEN <EDBEK@YAHOO.COM>
2020-09-18 18:38:19 -0500

    Added function to pull from Spotify API and return all media data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import hash from "./hash";
+import * as $ from "jquery";
+import Player from "./Player";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -21,6 +23,21 @@ const hash = window.location.hash
   }, {});
 
   class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        token: null,
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms:0,
+      },
+      is_playing: "Paused",
+      progress_ms: 0
+    };
     componentDidMount() {
       // Set token
       let _token = hash.access_token;
@@ -31,6 +48,27 @@ const hash = window.location.hash
         });
       }
     }
+
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }
+  
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: (data) => {
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+        });
+      }
+    });
+  }
 
     render() {
       return (
