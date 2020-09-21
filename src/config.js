2ec7a1a5e3d2c512169a2a4b91cd3209fae7634e
EDEN <EDBEK@YAHOO.COM>
2020-09-21 13:02:32 -0500

    Debugged authentication login issues

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const authEndpoint =
+  "https://accounts.spotify.com/authorize/?client_id=a7cea9e91767484796676a5037d4375c&redirect_uri=https://localhost:3000/callback/&scope=user-read-currently-playing%20user-read-playback-state&response_type=token&show_dialog=true";
+
+export const clientId = "a7cea9e91767484796676a5037d4375c";
+export const redirectUri = "https://localhost:3000/callback/";
+export const scopes = ["user-read-currently-playing", "user-read-playback-state"];
